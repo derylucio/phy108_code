d716dd0fdc01bfe35dcaed8f46e6cdb15680f9cc
Mirae Parker <Mirae Parker>
2017-05-16 10:17:45 -0700

    Changed database name

--- tc_calibration.py
+++ tc_calibration.py
@@ -13,7 +13,7 @@ from time import gmtime, strftime
 #Need to put third instrument into the following list:
 TEMP_ADAPTERS = ["GPIB0::2::INSTR" , "GPIB0::3::INSTR"]  
 TIME_DELAY = 0.1
-OUTPUT_FILE = "tc_calibration_file" + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ".txt" # puts timestamp on filename
+OUTPUT_FILE = "tc_calibration_file" + strftime("%Y-%m-%d %H-%M-%S", gmtime()) + ".txt" # puts timestamp on filename
 
  
 if __name__ == "__main__":
